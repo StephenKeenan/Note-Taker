8c08b9e9a89ff77410d947f8b80f0022204d382f
Stephen Keenan <stephenkeenan02@gmail.com>
2020-12-05 18:54:09 -0500

    server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 const db = require("./db/db.json");
 
 let note = [];
+const {v4 : uuidv4} = require('uuid')
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -28,3 +29,17 @@ app.get("/notes", function (req, res){
 app.get("/api/notes", function (req, res) {
     return res.json(JSON.parse(fs.readFileSync("/.db/db.json")))
 });
+
+app.post("/api/notes", (req, res) => {
+  let note = req.body;
+  let db = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  note.id = uuidv4();
+  db.push(note);
+  fs.writeFileSync ("./db/db.json", JSON.stringify(db));
+  res.json(note);
+});
+
+app.delete("/api/notes/:id", function (req, res){
+    let id = (req.params.id);
+    let db = JSON.parse(fs.readFileSync("./db/db.json", "utf8"))
+})
