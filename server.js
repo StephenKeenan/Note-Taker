7c0a5d1fa16bc6e3c13763888fe1a94d30eed50a
Stephen Keenan <stephenkeenan02@gmail.com>
2020-12-05 18:38:39 -0500

    server.js

--- server.js
+++ server.js
@@ -7,3 +7,24 @@ const PORT = process.env.PORT || 3000;
 const db = require("./db/db.json");
 
 let note = [];
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, function () {
+    console.log("Server is listening: " + PORT)
+});
+
+
+app.get("*", function (req, res){
+    res.sendFile(path.join(__dirname, "public/assets/js/index.js"));
+});
+
+app.get("/notes", function (req, res){
+    res.sendFile(path.join(__dirname, "public/notes.html"))
+});
+
+app.get("/api/notes", function (req, res) {
+    return res.json(JSON.parse(fs.readFileSync("/.db/db.json")))
+});
