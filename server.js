94921c8960665912a7c89042171ed4444b944d24
Stephen Keenan <stephenkeenan02@gmail.com>
2020-12-07 11:00:29 -0500

    typo fix

--- server.js
+++ server.js
@@ -50,5 +50,5 @@ app.delete('/api/notes/:id', function (req, res) {
 });
 
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname + './public/indexx.html'));
+    res.sendFile(path.join(__dirname + './public/index.html'));
 });
