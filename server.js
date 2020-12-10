e66558020ad09152d0712c293fa59df76af6ea87
Stephen Keenan <stephenkeenan02@gmail.com>
2020-12-09 19:51:01 -0500

    Formatting

--- server.js
+++ server.js
@@ -1,54 +1,52 @@
 const express = require('express');
-const path = require("path");
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const db = require("./db/db.json");
+const db = require('./db/db.json');
 let note = [];
 const { v4: uuidv4 } = require('uuid')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-
-//Listening function to the local host
 app.listen(PORT, function () {
-    console.log("Server is listening: http://localhost:" + PORT);
+    console.log('Server is listening: http://localhost:' + PORT);
 });
 
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/assets/js/index.js"));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public/assets/js/index.js'));
 }); 
 
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"))
+app.get('/notes', function (req, res) {
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
 }); 
 
-app.get("/api/notes", function (req, res) {
+app.get('/api/notes', function (req, res) {
     return res.json(JSON.parse(fs.readFileSync("./db/db.json")));
 });
 
 
-app.post("/api/notes", (req, res) => {
+app.post('/api/notes', (req, res) => {
     let note = req.body;
-    let db = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let db = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
     note.id = uuidv4();
     db.push(note);
-    fs.writeFileSync("./db/db.json", JSON.stringify(db));
+    fs.writeFileSync('./db/db.json', JSON.stringify(db));
     res.json(note);
 });
 
-app.delete("/api/notes/:id", function (req, res) {
+app.delete('/api/notes/:id', function (req, res) {
     let id = (req.params.id);
-    let db = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let db = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
     let removeNotes = db.filter(items => items.id != id);
-    fs.writeFileSync("./db/db.json", JSON.stringify(removeNotes));
+    fs.writeFileSync('./db/db.json', JSON.stringify(removeNotes));
     res.json(true);
 });
 
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname + "./public/index.html"));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname + './public/index.html'));
 });
